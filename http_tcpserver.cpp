026d94d6ddfcddc830722f7873afe9f7b6142676
0x-anshu1 <aryananshul282@gmail.com>
2025-12-19 22:04:37 +0530

    server loop creation

--- http_tcpserver.cpp
+++ http_tcpserver.cpp
@@ -1,22 +1,80 @@
 #include<iostream>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include<string>
 
 #include"http_tcpserver.h"
 
 using namespace std;
 using namespace http;
 
+tcpserver::tcpserver(std::string ip_address, int port){
+	if(create_sock()==1){
+		cerr<<"Error: Create Socket"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	assign(ip_address,port);
+	if(bind(m_sock,(struct sockaddr*)&m_address,sizeof(m_address))<0){
+			cerr<<"Error: Bind Unsuccessful"<<endl;
+			perror("Bind");
+			exit(EXIT_FAILURE);
+	}
+
+	if(listen(m_sock,5)<0){
+		perror("listen");
+		cerr<<"Error: Listen Unsuccessful"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	while(true){
+		loop();
+	}
+
+}
+tcpserver::~tcpserver(){
+	close(m_sock);
+	//close(m_lsock);
+}
+
+void tcpserver::assign(std::string address,int port){
+	m_address.sin_family = AF_INET;
+	m_address.sin_port = htons(port);
+	if(inet_pton(AF_INET,address.c_str(),&m_address.sin_addr)!=1){
+		perror("inet_pton");
+		cerr<<"Error: Inet_pton Unsuccessful"<<endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
 int tcpserver::create_sock(){
-	m_soc = socket(AF_INET,SOCK_STREAM,0);
-	if(soc<0){
-		perror("SOCKET");
+	m_sock = socket(AF_INET,SOCK_STREAM,0);
+	if(m_sock<0){
 		return 1;
 	}
 	return 0;
 }
-void tcpserver::close_sock(){
-	close(m_soc);
+
+void tcpserver::loop(){
+	int lsock = accept(m_sock,NULL,NULL);
+	if(lsock<0){
+		perror("accept");
+		cerr<<"Error: Accept Problem."<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	ssize_t i = recv(lsock,m_buff,sizeof(m_buff),0);
+	if(i<=0){
+		perror("recv");
+		cerr<<"Error: Recv Problem"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	m_msg = process(m_buff);
+	i = send(lsock,m_msg.c_str(),m_msg.size(),0);
+	if(i<=0){
+		perror("send");
+		cerr<<"Error: Send Problem"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	close(lsock);
 }
--- http_tcpserver.h
+++ http_tcpserver.h
@@ -6,18 +6,24 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<unistd.h>
+#include<cstring>
+#include<string>
+#include<cstdlib>
 
 namespace http{
 	class tcpserver{
 		public:
-			tcpserver(struct sockaddr_in );
+			tcpserver(std::string ip_address, int port);
 			~tcpserver();
 			
 			
 		private:
-			int m_soc;
+			int m_sock;
+			char m_buff[5024];
 			struct sockaddr_in m_address;
+			void assign(std::string address,int port);
 			int create_sock();
-			void close_sock();
-	}
+			std::string m_msg;
+			std::string process(char& msg);
+	};
 }
